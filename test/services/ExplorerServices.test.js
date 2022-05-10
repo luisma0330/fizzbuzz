3ffdcd44161e9be4e49ee8f16175fe0b2801a792
luisma0330 <luisma0330@gmail.com>
2022-05-10 15:37:52 -0500

    Agregando jest y pruebas de unidad para ExplorerServices y FizzbuzzServices

---
+++ test/services/ExplorerServices.test.js
@@ -0,0 +1,19 @@
+const ExplorerService = require("../../lib/services/ExplorerService")
+
+describe("Test para ExplorerService", () => {
+    test("Requerimiento 1: Calcular todos los explorers en la mission Node", () => {
+        const explorers = [{mission: "node"}, {mission: "node"}, {mission: "java"}]
+        const explorersInNode = ExplorerService.filterByMission(explorers, "node")
+        expect(explorersInNode.length).toBe(2)
+    })
+    test("Requerimiento 2: Obtener la cantidad de exploradores en la mission node", ()  => {
+        const explorers = [{mission: "node"}, {mission: "node"}, {mission: "java"}];
+        const explorersInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node")
+        expect(explorersInNode).toBe(2)
+    })
+    test("Requerimiento 3: Obtener los GitHub usernames de los explorers por mission", () => {
+        const explorers = [{"mission": "node", "githubUsername": "explorer1"}, {"mission": "node", "githubUsername": "explorer2"},{"mission": "java", "githubUsername": "explorer3"}];
+        const usernames = ExplorerService.getExplorersUsernamesByMission(explorers, "node")
+        expect(usernames).toEqual(["explorer1", "explorer2"])
+    })
+})
