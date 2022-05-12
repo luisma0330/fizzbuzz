335f6b868e3b99e19bd3e6ed35fb9bb4095621b8
luisma0330 <luisma0330@gmail.com>
2022-05-12 15:26:41 -0500

    Agregando la metodo para regresar los usernames

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -9,4 +9,9 @@ describe("Tests para ExplorerController", () => {
         const explorersInJava = ExplorerController.getExplorersAmountByMission("java");
         expect(explorersInJava).toBe(5);
     });
+    test("3) Obtener los usernames de los explorers por mission", () => {
+        const explorersInNode = ExplorerController.getExplorersUsernamesByMission("node");
+        const expected = ["ajolonauta1", "ajolonauta2", "ajolonauta3", "ajolonauta4", "ajolonauta5", "ajolonauta11","ajolonauta12", "ajolonauta13", "ajolonauta14", "ajolonauta15"];
+        expect(explorersInNode).toEqual(expected);
+    });
 });
