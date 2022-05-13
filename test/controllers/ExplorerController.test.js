2915e371ed990bdb0b99b7b2640f6a038ef122b6
luisma0330 <luisma0330@gmail.com>
2022-05-12 19:31:40 -0500

    Añadiendo la funcion de validacion de fizzbuzz al ExplorerController

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,4 +1,5 @@
 const ExplorerController = require("../../lib/controllers/ExplorerController");
+const FizzbuzzService = require("../../lib/services/FizzbuzzService");
 
 describe("Tests para ExplorerController", () => {
     test("1) Obtener explorers por mission", () => {
@@ -13,5 +14,9 @@ describe("Tests para ExplorerController", () => {
         const explorersInNode = ExplorerController.getExplorersUsernamesByMission("node");
         const expected = ["ajolonauta1", "ajolonauta2", "ajolonauta3", "ajolonauta4", "ajolonauta5", "ajolonauta11","ajolonauta12", "ajolonauta13", "ajolonauta14", "ajolonauta15"];
         expect(explorersInNode).toEqual(expected);
+    });
+    test("4) Obtener la validación fizzbuzz de un numero dado", () => {
+        const trick = FizzbuzzService.applyValidationInNumber(8);
+        expect(trick).toBe(8);
     });
 });
